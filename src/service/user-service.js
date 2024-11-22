ad52f4a9b39a064b968caeec10f528e9e4b99e99
sultansyah <muhammadsultansyah14@gmail.com>
2024-11-23 00:18:33 +0700

    feat: add controller service user register

---
+++ src/service/user-service.js
@@ -0,0 +1,35 @@
+import { registerUserValidation } from "../validation/user-validation.js"
+import { validate } from "../validation/validation.js"
+import { prismaClient } from "../application/database.js"
+import { ResponseError } from "../error/response-error.js"
+import bcrypt from "bcrypt";
+
+const register = async (request) => {
+    const user = validate(registerUserValidation, request)
+
+    const countUser = await prismaClient.user.count({
+        where: {
+            username: user.username
+        }
+    })
+
+    if(countUser === 1) {
+        throw new ResponseError(400, "Username already exists")
+    }
+
+    user.password = await bcrypt.hash(user.password, 10)
+    console.log(user);
+    
+
+    return prismaClient.user.create({
+        data: user,
+        select: {
+            username: true,
+            name: true
+        }
+    })
+}
+
+export default {
+    register
+}
