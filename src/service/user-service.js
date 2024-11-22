5af0e3728c60707f4929d384beb355e7a4efbb39
sultansyah <muhammadsultansyah14@gmail.com>
2024-11-23 00:37:25 +0700

    feat: add endpoint user login

--- src/service/user-service.js
+++ src/service/user-service.js
@@ -1,8 +1,9 @@
-import { registerUserValidation } from "../validation/user-validation.js"
+import { loginUserValidation, registerUserValidation } from "../validation/user-validation.js"
 import { validate } from "../validation/validation.js"
 import { prismaClient } from "../application/database.js"
 import { ResponseError } from "../error/response-error.js"
-import bcrypt from "bcrypt";
+import bcrypt from "bcrypt"
+import { v4 as uuid } from "uuid"
 
 const register = async (request) => {
     const user = validate(registerUserValidation, request)
@@ -13,13 +14,13 @@ const register = async (request) => {
         }
     })
 
-    if(countUser === 1) {
+    if (countUser === 1) {
         throw new ResponseError(400, "Username already exists")
     }
 
     user.password = await bcrypt.hash(user.password, 10)
     console.log(user);
-    
+
 
     return prismaClient.user.create({
         data: user,
@@ -30,6 +31,43 @@ const register = async (request) => {
     })
 }
 
+const login = async (request) => {
+    const loginRequest = validate(loginUserValidation, request)
+
+    const user = await prismaClient.user.findUnique({
+        where: {
+            username: loginRequest.username
+        },
+        select: {
+            username: true,
+            password: true
+        }
+    })
+
+    if(!user) {
+        throw new ResponseError(401, "Username or password wrong")
+    }
+
+    const isPasswordValid = await bcrypt.compare(loginRequest.password, user.password)
+    if(!isPasswordValid) {
+        throw new ResponseError(401, "Username or password wrong")
+    }
+
+    const token = uuid().toString()
+    return await prismaClient.user.update({
+        data: {
+            token: token
+        },
+        where: {
+            username: user.username
+        },
+        select: {
+            token: true
+        }
+    })
+}
+
 export default {
-    register
+    register,
+    login
 }
