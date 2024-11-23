3c429a64a7bb6817f57c01f94488f1a10879039c
sultansyah <muhammadsultansyah14@gmail.com>
2024-11-23 12:18:46 +0700

    feat: add endpoint get address

--- src/service/address-service.js
+++ src/service/address-service.js
@@ -1,4 +1,4 @@
-import { createAddressValidation } from "../validation/address-validation.js"
+import { createAddressValidation, getAddressValidation } from "../validation/address-validation.js"
 import { getContactValidation } from "../validation/contact-validation.js"
 import { validate } from "../validation/validation.js"
 import { prismaClient } from "../application/database.js"
@@ -8,7 +8,7 @@ import { v4 as uuid } from "uuid"
 import { request } from "express"
 
 
-const create = async (user, contactId, request) => {
+const checkContactMustExists = async (user, contactId) => {
     contactId = validate(getContactValidation, contactId)
 
     const totalContactInDatabase = await prismaClient.contact.count({
@@ -21,6 +21,13 @@ const create = async (user, contactId, request) => {
     if(totalContactInDatabase !== 1) {
         throw new ResponseError(404, "contact is not found")
     }
+
+    return contactId
+}
+
+
+const create = async (user, contactId, request) => {
+    contactId = await checkContactMustExists(user, contactId)
 
     const address = validate(createAddressValidation, request)
     address.contact_id = contactId
@@ -38,6 +45,33 @@ const create = async (user, contactId, request) => {
     })
 }
 
+const get = async (user, contactId, addressId) => {
+    contactId = await checkContactMustExists(user, contactId)
+    addressId = validate(getAddressValidation, addressId)
+
+    const address = await prismaClient.address.findFirst({
+        where:{
+            contact_id: contactId,
+            id: addressId
+        },
+        select: {
+            id: true,
+            street: true,
+            city: true,
+            province: true,
+            country: true,
+            postal_code: true,
+        }
+    })
+
+    if(!address) {
+        throw new ResponseError(404, "address is not found")
+    }
+
+    return address
+}
+
 export default{
-    create
+    create,
+    get,
 }
