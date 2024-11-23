dbad451378d930f4115d5683d3b2be33623071c0
sultansyah <muhammadsultansyah14@gmail.com>
2024-11-23 12:40:36 +0700

    feat: add endpoint delete address

--- src/service/address-service.js
+++ src/service/address-service.js
@@ -106,10 +106,33 @@ const update = async (user, contactId, request) => {
             postal_code: true,
         }
     })
-} 
+}
+
+const remove = async (user, contactId, addressId) => {
+    contactId = await checkContactMustExists(user, contactId)
+    addressId = validate(getAddressValidation, addressId)
+
+    const totalAddressInDatabase = await prismaClient.address.count({
+        where:{
+            contact_id: contactId,
+            id: addressId
+        }
+    })
+
+    if(totalAddressInDatabase !== 1) {
+        throw new ResponseError(404, "address is not found")
+    }
+
+    return await prismaClient.address.delete({
+        where: {
+            id: addressId
+        }
+    })
+}
 
 export default{
     create,
     get,
-    update
+    update,
+    remove
 }
